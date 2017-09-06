d998e7b78647e37ff9c388d3a43a8dae9b9a6877
Chanel Aquino <caquino@csumb.edu>
2017-09-06 01:49:39 +0000

    modified template files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' , className: 'CST438'}); // render function: use template index (located in views director: index.jade)
 });
 
 module.exports = router;
